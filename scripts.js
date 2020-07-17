1199560f529e9a24c38c3c2c495ea1f4fa0e4787
ruthjomo <ruthjomo19@gmail.com>
2020-07-17 13:27:50 +0300

    added codes in Js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 var a=new Date("input");
 alert(a.getDay());
+var dayNames
+var male
+var female
+dayNames=["Sunday: Akosua","Monday: Adwoa","Tuesday: Abenaa","Wednesday: Akua","Thursday:Yaa",
+               "Friday: Afua","Saturday: Ama","Sunday: Kwasi","Monday: Kwadwo","Tuesday: Kwabena",
+               "Wednesday: Kwaku","Thursday:Yaw","Friday: Kofi","Saturday: Kwame"]
+male=["Sunday: Kwasi","Monday: Kwadwo","Tuesday: Kwabena",
+"Wednesday: Kwaku","Thursday:Yaw","Friday: Kofi","Saturday: Kwame"]
+female=["Sunday: Akosua","Monday: Adwoa","Tuesday: Abenaa","Wednesday: Akua","Thursday:Yaa",
+"Friday: Afua","Saturday: Ama"]
+if(female=true)
+
+
+              
+            
+            
+            
+
  
 // var gender=female
 // var aknanName=["Sunday: Akosua","Monday: Adwoa","Tuesday: Abenaa","Wednesday: Akua","Thursday:Yaa",
