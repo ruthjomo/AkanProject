c6300ed1cd179dc22c2817d0995e67bf8a88f832
ruthjomo <ruthjomo19@gmail.com>
2020-07-17 12:50:14 +0300

    my initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var a=new Date("input");
+alert(a.getDay());
+ 
+// var gender=female
+// var aknanName=["Sunday: Akosua","Monday: Adwoa","Tuesday: Abenaa","Wednesday: Akua","Thursday:Yaa",
+//                "Friday: Afua","Saturday: Ama" ]
+//     function(aknanName);
+// female=true
+// if(female){
+//     (a.getDay(aknanName));
+// }
