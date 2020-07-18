7d50f82adce018c3547d4c5d9bb51e0441bbb21b
ruthjomo <ruthjomo19@gmail.com>
2020-07-18 13:44:31 +0300

    changed some content in scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function getAkanName () {
 		}
 	  }
 	}
-  // let myGenderValue = getGender();
+   let myGenderValue = getGender();
   console.log(getGender);
   
 	// validation functions
@@ -26,7 +26,7 @@ function getAkanName () {
 	}
   function dayValidator () {
 	  if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
-		if (dayOfBirth > 28 || dayOfBirth < 1) {
+		if (dayOfBirth > 29 || dayOfBirth < 1) {
 		  return false;
 		} else if (monthOfBirth === 2 && dayOfBirth > 29) {
 		  return false;
@@ -58,43 +58,43 @@ function getAkanName () {
 	if (myGenderValue == "male" && monthValid && dayValid){
 	  switch (dayOfWeekNumber) {
 		case 1:
-		  document.getElementById('result').textContent = "Hello there!You were born on a Sunday: your Akan name is " + maleAkanNames[0];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + maleAkanNames[0];
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 2:
 		  document.getElementById('result').textContent = "You were born on a Monday: your Akan name is " + maleAkanNames[1];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 3:
 		  document.getElementById('result').textContent = "You were born on a Tuesday: your Akan name is " + maleAkanNames[2];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 4:
 		  document.getElementById('result').textContent = "You were born on a Wednesday: your Akan name is " + maleAkanNames[3];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 5:
 		  document.getElementById('result').textContent = "You were born on a Thursday: your Akan name is " + maleAkanNames[4];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 6:
 		  document.getElementById('result').textContent = "You were born on a Friday: your Akan name is " + maleAkanNames[5];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 7:
 		  document.getElementById('result').textContent = "You were born on a Saturday: your Akan name is " + maleAkanNames[6];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 0:
 		  document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + maleAkanNames[0];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		default:
@@ -105,43 +105,43 @@ function getAkanName () {
 	  switch (dayOfWeekNumber) {
 		case 1:
 		  document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + femaleAkanNames[0];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 2:
 		  document.getElementById('result').textContent = "You were born on a Monday: your Akan name is " + femaleAkanNames[1];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  alert("Kwadwo");
 		  break;
 		case 3:
 		  document.getElementById('result').textContent = "You were born on a Tuesday: your Akan name is " + femaleAkanNames[2];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 4:
 		  document.getElementById('result').textContent = "You were born on a Wednesday: your Akan name is " + femaleAkanNames[3];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 5:
 		  document.getElementById('result').textContent = "You were born on a Thursday: your Akan name is " + femaleAkanNames[4];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 6:
 		  document.getElementById('result').textContent = "You were born on a Friday: your Akan name is " + femaleAkanNames[5];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 7:
 		  document.getElementById('result').textContent = "You were born on a Saturday: your Akan name is " + femaleAkanNames[6];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		case 0:
 		  document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + femaleAkanNames[0];
-		  document.getElementById('display-name').textContent = "Here is your Akan name: ";
+		  document.getElementById('display-name').textContent = "Hello there!: ";
 		  return false;
 		  break;
 		default:
