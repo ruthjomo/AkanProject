26c2687b9865d09d792be54541da0550c8f438a5
ruthjomo <ruthjomo19@gmail.com>
2020-07-18 13:01:51 +0300

    Made changes in my scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function getAkanName () {
-	let yearOfBirth = document.getElementById("year-input").value;
-	let monthOfBirth = Number(document.getElementById("month-input").value);
-	let dayOfBirth = Number(document.getElementById("day-input").value);
+	var yearOfBirth = document.getElementById("year-input").value;
+	var monthOfBirth = Number(document.getElementById("month-input").value);
+	var dayOfBirth = Number(document.getElementById("day-input").value);
   
 	let genders = document.getElementsByName("gender");
   
